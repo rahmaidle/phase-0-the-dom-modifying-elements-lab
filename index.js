f0ef139ad535a14b8f28522fe1626a1d050d50af
rahmaidle <rahmaidle254@gmail.com>
2024-06-28 10:54:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove("main#main");
+
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent=  "Rahma is the champion";
+
+document.body.appendChild(newHeader);
